5695e4971a219e57f77ec5a1fee14c96293751a6
TadasR <tadas.rumsas@gmail.com>
2024-11-27 12:43:28 +0200

    add 1-uzd 27.11.2024

---
+++ react/27.11.2024/1-uzduotis/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card"
+
+function App() {
+
+  return (
+<main className ="container">
+  <div className="row">
+     <Card />
+     </div>
+     </main>
+  )
+}
+
+export default App
